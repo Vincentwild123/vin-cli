c5c49a6e5af5385fda5e34b70c4d133955b9f6e0
vincent <18wjchen1@stu.edu.cn>
2021-04-18 19:51:10 +0800

    update repo url

--- utils/spinner.js
+++ utils/spinner.js
@@ -1,9 +1,11 @@
 const ora = require('ora')
 const config = {
     text: 'downloading',
-    hideCursor: true,
+    hideCursor: false,
     color: 'white',
 }
 const spinner = ora(config);
 
 module.exports = spinner;
+
+
